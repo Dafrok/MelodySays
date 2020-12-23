dc11aaed147057170a3d7d652a9d41a343448374
Dafrok <o.o@mug.dog>
2020-12-23 19:56:15 +0800

    web socket version

--- client/textbox/index.js
+++ client/textbox/index.js
@@ -3,29 +3,6 @@
  * @author Dafrok
  */
 
-const {ipcRenderer} = require('electron');
-const fs = require('fs');
-const path = require('path');
-const clipIt = require('./clip-it');
-
-var ni = require('os').networkInterfaces();
-function getLocalIp() {
-    var ipAddress = [];
-    for (var key in ni) {
-        for (var index in ni[key]) {
-            if (ni[key][index].family === 'IPv4' && !ni[key][index].internal) {
-                ipAddress.unshift(ni[key][index]);
-            }
-        }
-    }
-    if (ipAddress.length) {
-        return ipAddress;
-    }
-    else {
-        return ['127.0.0.1'];
-    }
-}
-
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -48,15 +25,6 @@ class App extends san.Component {
                     <input type="checkbox" checked="{= noBorderMode =}">
                 </div>
             </div>
-            <div class="field">
-                <label>本地IP</label>
-                <div class="static">
-                    <p>在下列地址中复制复制并发送到同一局域网下的手机中用浏览器打开（有效地址通常为 192.168.0 或 192.168.1 开头）</p>
-                    <ul>
-                        <li s-for="item in ip"><a on-click="copyText(item.address + ':3080')">{{item.address}}:3080 <i class="fa fa-copy"></i></a></li>
-                    </ul>
-                </div>
-            </div>
         </form>
     </div>
     <div id="content" s-else>
@@ -73,39 +41,14 @@ class App extends san.Component {
     <div class="menu">
         <span class="rest"></span>
         <a class="menu-btn" on-click="toggleSetting"><i class="fa fa-wrench"></i></a>
-        <a class="menu-btn" on-click="minimize"><i class="fa fa-minus"></i></a>
-        <a class="menu-btn" on-click="quit"><i class="fa fa-times"></i></a>
     </div>
     <div class="menu-shadow">
         <span class="rest"></span>
         <a class="menu-btn"><i class="fa fa-wrench"></i></a>
-        <a class="menu-btn"><i class="fa fa-minus"></i></a>
-        <a class="menu-btn"><i class="fa fa-times"></i></a>
     </div>
     <div class="{{!outputing && content.length > 1 ? '' : 'hidden'}} next"></div>
 </div>
     `;
-    copyText(str) {
-        if (clipIt(str)) {
-            alert(`"${str}" 复制成功`);
-        }
-        else {
-            alert('复制失败');
-        };
-    }
-    minimize() {
-        ipcRenderer.send('minimize');
-    }
-    quit() {
-        if (confirm('拜拜~')) {
-            localStorage.setItem('username', this.data.get('name'));
-            localStorage.setItem('noBorderMode', this.data.get('noBorderMode'));
-            ipcRenderer.send('close');
-        }
-    }
-    getIp() {
-        this.data.set('ip', getLocalIp());
-    }
     changeName() {
         prompt('My Melody');
     }
@@ -151,9 +94,9 @@ class App extends san.Component {
         this.data.set('showSetting', !this.data.get('showSetting'));
     }
     attached() {
-        this.getIp();
         this.textQueue = [];
-        ipcRenderer.on('record', (e, message) => {
+        const socket = io();
+        socket.on('output', message => {
             this.textQueue.push(...message);
             this.startOutputTextQueue();
         });
@@ -174,10 +117,3 @@ class App extends san.Component {
 
 const app = new App();
 app.attach(document.body);
-
-(async () => {
-    const watcher = fs.watch(path.resolve(__dirname));
-    watcher.on('change', () => {
-        ipcRenderer.send('re-render');
-    });
-})();
---
+++ server/start.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 80;
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const publicPath = path.resolve(__dirname, '../client');
+
+app.set('public', publicPath);
+app.use(express.static(publicPath));
+app.use(bodyParser.text());
+
+io.on('connection', socket => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('record', msg => {
+        console.log('record!', msg);
+        socket.broadcast.emit('output', msg);
+    });
+});
+
+http.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
